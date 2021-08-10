2b350aeb95d3c336b1d5729648d687a805f86599
rahul <rahulrajcse07@gmail.com>
2021-08-10 12:23:35 +0530

    emoji added check

--- demo/Assets/js/index.js
+++ demo/Assets/js/index.js
@@ -5,6 +5,7 @@ const cardItems = [
     imgAlt: "angular-have",
     demoUrl: "https://angular-notepad-classic.web.app/sign-in",
     sourceUrl: "https://github.com/rahul162raj/angular-notepad-classic",
+    status:"PUBLIC",
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ const cardItems = [
     imgAlt: "react-shop",
     demoUrl: "https://rahul162raj.github.io/react-shopping-app/",
     sourceUrl: "https://github.com/rahul162raj/react-shopping-app-starter-kit",
+    status:"PUBLIC",
   },
   {
     id: 3,
@@ -20,6 +22,7 @@ const cardItems = [
     demoUrl: "https://rahul162raj.github.io/pongal/",
     sourceUrl:
       "https://github.com/rahul162raj/rahul162raj.github.io/tree/master/pongal",
+    status:"WIP",
   },
   {
     id: 4,
@@ -27,6 +30,7 @@ const cardItems = [
     imgAlt: "css-challenge",
     demoUrl: "https://rahul162raj.github.io/css-challenge/#/day-1",
     sourceUrl: "https://github.com/rahul162raj/100-days-css-challenge",
+    status:"PUBLIC",
   },
   {
     id: 5,
@@ -34,8 +38,19 @@ const cardItems = [
     imgAlt: "web-swevens",
     demoUrl: "https://www.swevens.co/index.html",
     sourceUrl: "https://github.com/rahul162raj/100-days-css-challenge",
+    status:"PRIVATE",
   },
 ];
+
+function checkStatus(item){
+  if(item.status === "WIP"){
+    return `<span class="font-mr">&#128679;</span><span class="footer-text">WIP</span>`
+  } else if(item.status === "PRIVATE"){
+    return `<span class="font-mr">&#128274;</span><span class="footer-text">PRIVATE</span>`
+  } else{
+    return `<a href=${item.sourceUrl}  class="footer-text">View Source</a>`
+  }
+}
 
 let cardElements = `${cardItems
   .map(
@@ -43,7 +58,7 @@ let cardElements = `${cardItems
     <div class="card">
         <img src=${item.img} alt="${item.imgAlt}" class="card-img">
         <div class="card-footer">
-            <a href=${item.sourceUrl}  class="footer-text">View Source</a>
+            ${checkStatus(item)}
         </div>
     </div>
     `
