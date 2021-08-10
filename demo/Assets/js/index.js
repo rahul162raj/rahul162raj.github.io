2c6eb240d01555c41d0604e1478997672e18a214
rahulrajnataraj <38761978+rahul162raj@users.noreply.github.com>
2021-08-10 11:13:00 +0530

    relative path updated

--- demo/Assets/js/index.js
+++ demo/Assets/js/index.js
@@ -1,21 +1,21 @@
 const cardItems = [
   {
     id: 1,
-    img: "/Assets/images/angular-have.png",
+    img: "./Assets/images/angular-have.png",
     imgAlt: "angular-have",
     demoUrl: "https://angular-notepad-classic.web.app/sign-in",
     sourceUrl: "https://github.com/rahul162raj/angular-notepad-classic",
   },
   {
     id: 2,
-    img: "/Assets/images/react-shop.png",
+    img: "./Assets/images/react-shop.png",
     imgAlt: "react-shop",
     demoUrl: "https://rahul162raj.github.io/react-shopping-app/",
     sourceUrl: "https://github.com/rahul162raj/react-shopping-app-starter-kit",
   },
   {
     id: 3,
-    img: "/Assets/images/css-pongal.png",
+    img: "./Assets/images/css-pongal.png",
     imgAlt: "css-pongal",
     demoUrl: "https://rahul162raj.github.io/pongal/",
     sourceUrl:
@@ -23,14 +23,14 @@ const cardItems = [
   },
   {
     id: 4,
-    img: "/Assets/images/css-challenge.png",
+    img: "./Assets/images/css-challenge.png",
     imgAlt: "css-challenge",
     demoUrl: "https://rahul162raj.github.io/css-challenge/#/day-1",
     sourceUrl: "https://github.com/rahul162raj/100-days-css-challenge",
   },
   {
     id: 5,
-    img: "/Assets/images/web-swevens.png",
+    img: "./Assets/images/web-swevens.png",
     imgAlt: "web-swevens",
     demoUrl: "https://www.swevens.co/index.html",
     sourceUrl: "https://github.com/rahul162raj/100-days-css-challenge",
